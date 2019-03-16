8dfc9742ee5755edbc9d2d603e165debc3797c0f
Andrew Erskine <erskine94@gmail.com>
2019-03-15 21:28:45 -0700

    repeat play bug fixed

--- script.js
+++ script.js
@@ -11,22 +11,28 @@ function getOrCreateContext() {
         context = new AudioContext();
         oscillator = context.createOscillator();
         oscillator.connect(context.destination);
+        oscillator.start(0)
     }
     return context;
 }
 
-/* set up event listeners */
-document.getElementById('play').addEventListener('click', playSequence);
-
 function playSequence() {
     getOrCreateContext();
-    oscillator.start(0);
+    // oscillator.start(0);
     let time = context.currentTime + eps;
     sequence.forEach(note => {
-        const freq = Math.pow(2, (note[0]-69)/12)*440;
+        let freq = midiToFreq(note[0]);
         console.log(time);
         oscillator.frequency.setTargetAtTime(0, time - eps, 0.001);
         oscillator.frequency.setTargetAtTime(freq, time, 0.001);
         time += length / note[1];
     });
+
 }
+
+function midiToFreq(midiNote) {
+    return Math.pow(2, (midiNote-69)/12)*440;
+}
+
+/* set up event listeners */
+document.getElementById('play').addEventListener('click', playSequence);
